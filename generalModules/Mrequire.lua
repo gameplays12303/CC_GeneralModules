067f265e109fbcbe167b409d4fa19331c6cf38ea
wendell_Parham <wendell.parham@outlook.com>
2023-08-03 20:39:31 -0500

    Update Mrequire.lua

--- generalModules/Mrequire.lua
+++ generalModules/Mrequire.lua
@@ -4,8 +4,9 @@
 -- unless the reload boolean is set to true then it will simply reload it 
 -- if the module fails to load it will not save it in the loaded Table unlike the actual require
 
-local util = require and require("generalModules.utilties") or dofile("generalModules/utilties.lua")
-local fm = require and require("generalModules.fm") or dofile("generalModules/fm.lua")
+local util = require and require("generalModules.utilties") or dofile("main/OS/install/parts/utilties.lua")
+local fm = require and require("generalModules.fm") or dofile("main/OS/install/parts/fm.lua")
+local expect = (require and require("cc.expect") or dofile("rom/modules/main/cc/expect.lua")).expect
 local handle = {}
 local Paths = util.file.list("rom/modules",false,true,true)
 table.insert(Paths,"")
@@ -14,6 +15,7 @@ handle.loaded = {}
 -- adds a LoadPath to the loadPaths Table
 ---@overload fun(_sPath:string,bReload:boolean)
 function handle.Path.Add(_sPath)
+    expect(1,_sPath,"string")
     if not fs.exists(_sPath)
     then
         error(("%s:not found"):format(_sPath),0)
@@ -23,6 +25,7 @@ end
 -- removes a loadPath from the loadPaths Table
 ---@overload fun(_sPath:string,bReload:boolean)
 function handle.Path.Remove(_sPath)
+    expect(1,_sPath,"string")
     local i = util.table.find(Paths,_sPath)
     if i
     then
@@ -30,8 +33,12 @@ function handle.Path.Remove(_sPath)
     end
     return true
 end
----@overload fun(_sPath:string,bReload:boolean)
-function handle.require(_sPath,bReload)
+---@overload fun(_sPath:string,_Env:table,bOneLoad:boolean|nil,bReload:boolean)
+function handle.require(_sPath,_Env,bOneLoad,bReload)
+    expect(1,_sPath,"string")
+    expect(2,_Env,"table","nil")
+    expect(3,bOneLoad,"boolean","nil")
+    expect(4,bReload,"boolean","nil")
     -- backwards support for require
     -- turns the '.' into "/" then adds
     -- .lua to the end of the string
@@ -76,7 +83,7 @@ function handle.require(_sPath,bReload)
         end
     end
     local SFn = fm.readFile(Path,"R")
-    local fn,err = load(SFn,"@"..Path,"bt",_ENV)
+    local fn,err = load(SFn,"@"..Path,"bt",_Env or setmetatable({["mReq"] = handle,["require"] = handle.require},{__index = _G}))
     if not fn
     then
         error(err,0)
@@ -86,7 +93,10 @@ function handle.require(_sPath,bReload)
     then
         error(ok[2],0)
     end
-    handle.loaded[_sPath] = fn
+    if not bOneLoad
+    then
+        handle.loaded[_sPath] = fn
+    end
     return table.unpack(ok,2)
 end
 return handle
